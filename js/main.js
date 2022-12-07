4e643173df53c161828aa835622e651ad6a0d25d
SkvCorpStory <119807198+SkvCorpStory@users.noreply.github.com>
2022-12-08 01:39:04 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,5 +3,6 @@ const contacts = document.querySelectorAll('.left>div');
     contacts.forEach(element => {
         element.addEventListener('click', function() {
             element.classList.toggle('active');
+            console.log(element);
         })
     })
